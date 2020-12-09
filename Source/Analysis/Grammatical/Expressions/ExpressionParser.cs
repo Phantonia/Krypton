73435c146e0c48f0755228a0380bf322b07fb7fa
Antonia Heinen <antonia.ph.heinen@gmail.com>
2020-12-09 22:23:11 +0100

    ...

---
+++ Source/Analysis/Grammatical/Expressions/ExpressionParser.cs
@@ -0,0 +1,45 @@
+﻿using Krypton.Analysis.AbstractSyntaxTree.Nodes.Expressions;
+using Krypton.Analysis.Lexical;
+using Krypton.Analysis.Lexical.Lexemes;
+using System;
+
+namespace Krypton.Analysis.Grammatical.Expressions
+{
+    internal class ExpressionParser
+    {
+        public ExpressionParser(LexemeCollection lexemes)
+        {
+            Lexemes = lexemes;
+        }
+
+        private int index = 0;
+
+        public int Index
+        {
+            get => index;
+            init => index = value;
+        }
+
+        public LexemeCollection Lexemes { get; }
+
+        public virtual ExpressionNode ParseNextExpression()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected ExpressionNode ParseSingleLexeme(Lexeme lexeme)
+        {
+
+        }
+    }
+
+    internal sealed class OperandExpressionParser : ExpressionParser
+    {
+        public OperandExpressionParser(LexemeCollection lexemes) : base(lexemes) { }
+
+        public override ExpressionNode ParseNextExpression()
+        {
+            return base.ParseNextExpression();
+        }
+    }
+}
