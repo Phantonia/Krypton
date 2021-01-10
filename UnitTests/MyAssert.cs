c209bf8930ff244d43a5e402a597030b31235b9d
Antonia Heinen <antonia.ph.heinen@gmail.com>
2021-01-10 19:51:48 +0100

    Added a custom Assert.Throws implementation, because the original one doesn't catch the exception which is a problem when debugging unit tests

---
+++ UnitTests/MyAssert.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTests
+{
+    public static class MyAssert
+    {
+        // For some reason, the original Assert.Throws<> method does not catch the exception.
+        // This makes debugging the tests a horror, so I wrote my own method.
+        public static TException? Throws<TException>(Action action)
+            where TException : Exception
+        {
+            try
+            {
+                action(); // this is expected to throw
+
+                Assert.Fail();
+                return null;
+            }
+            catch (TException ex)
+            {
+                return ex;
+            }
+        }
+    }
+}
