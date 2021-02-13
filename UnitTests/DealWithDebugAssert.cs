d29e969e999e7bb7b2bae71ba54e691061b7c6be
Antonia Heinen <antonia.ph.heinen@gmail.com>
2021-02-13 13:15:58 +0100

    Figured out how to fail unit test if Debug.Assert fails

---
+++ UnitTests/DealWithDebugAssert.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+
+namespace UnitTests
+{
+    [SetUpFixture]
+    class DealWithDebugAssert
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new AssertFailTraceListener());
+        }
+
+        private class AssertFailTraceListener : DefaultTraceListener
+        {
+            public AssertFailTraceListener() { }
+
+            public override void Fail(string? message)
+            {
+                Assert.Fail(message);
+            }
+
+            public override void Fail(string? message, string? detailMessage)
+            {
+                if (!string.IsNullOrWhiteSpace(detailMessage))
+                {
+                    Assert.Fail(message + " Details: " + detailMessage);
+                }
+
+                Assert.Fail(message);
+            }
+        }
+    }
+}
